07ab9f238f04324522f5d3478ffef15859baf515
Rob Golder <rgolder1@yahoo.com>
2012-10-23 15:02:50 +0100

    Completed MVC REST portion of webappdemo.

--- webappdemo/src/main/java/com/aztec/webappdemo/controller/MvcRestController.java
+++ webappdemo/src/main/java/com/aztec/webappdemo/controller/MvcRestController.java
@@ -7,13 +7,14 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.util.UriTemplate;
 
 import com.aztec.webappdemo.service.EntityService;
-import com.aztec.webappdemo.types.EntityCreation;
+import com.aztec.webappdemo.types.Entity;
 
 @Controller
 @RequestMapping("/mvc/rest")
@@ -31,13 +32,67 @@ public class MvcRestController {
     	this.service = service;
     }
     
-    @RequestMapping(value = URI_ENTITY, method = RequestMethod.POST, consumes = "application/json")
-    public ResponseEntity<String> createEntity(@RequestBody EntityCreation entityCreation) {
-    	service.put(entityCreation.getKey(), entityCreation.getValue());
-    	
-    	HttpHeaders httpHeaders = new HttpHeaders();
-        httpHeaders.setLocation(new UriTemplate(URI_ROOT + URI_ENTITY + "/" + entityCreation.getKey()).expand());
+    @RequestMapping(value=URI_ENTITY, method=RequestMethod.POST, consumes="application/json")
+    public ResponseEntity<String> createEntity(@RequestBody Entity entity) {
+        LOG.info("MvcRestController hit.  createEntity()");
+        ResponseEntity<String> responseEntity = null;
+        if(entity!=null && entity.getKey()!=0 && entity.getValue()!=null) {
+        	service.put(entity.getKey(), entity.getValue());
+        	HttpHeaders httpHeaders = new HttpHeaders();
+            httpHeaders.setLocation(new UriTemplate(URI_ROOT + URI_ENTITY + "/" + entity.getKey()).expand());
+        	responseEntity = new ResponseEntity<String>(httpHeaders, HttpStatus.CREATED);	
+        } else {
+        	responseEntity = new ResponseEntity<String>(HttpStatus.BAD_REQUEST);
+        }    	
 
-        return new ResponseEntity<String>(null, httpHeaders, HttpStatus.CREATED);
+        return responseEntity;
+    }
+    
+    @RequestMapping(value=URI_ENTITY+"/{key}", method=RequestMethod.GET, produces="application/json")
+    public ResponseEntity<Entity> getEntity(@PathVariable Long key) {
+    	LOG.info("MvcRestController hit.  getEntity()");
+    	String value = service.get(key);
+    	ResponseEntity<Entity> responseEntity = null;
+    	if(value!=null) {
+        	Entity entity = new Entity();
+        	entity.setKey(key);
+        	entity.setValue(value);
+    		responseEntity = new ResponseEntity<Entity>(entity, HttpStatus.OK);    			
+    	} else {
+    		// Key not found.
+    		responseEntity = new ResponseEntity<Entity>(HttpStatus.NOT_FOUND);
+    	}
+    	return responseEntity;
+    }
+    
+    @RequestMapping(value=URI_ENTITY+"/{key}", method=RequestMethod.PUT, consumes="application/json")
+    public ResponseEntity<String> updateEntity(@PathVariable Long key, @RequestBody Entity entity) {
+    	LOG.info("MvcRestController hit.  updateEntity()");
+    	ResponseEntity<String> responseEntity = null;
+    	if(entity==null || entity.getValue()==null) {
+        	responseEntity = new ResponseEntity<String>(HttpStatus.BAD_REQUEST);    		
+    	}
+    	if(service.containsKey(key)) {
+    		service.put(key, entity.getValue());
+    		responseEntity = new ResponseEntity<String>(HttpStatus.OK);
+    	} else {
+    		// Key not found.
+    		responseEntity = new ResponseEntity<String>(HttpStatus.NOT_FOUND);
+    	}
+    	return responseEntity;
+    }    
+    
+    @RequestMapping(value=URI_ENTITY+"/{key}", method=RequestMethod.DELETE)
+    public ResponseEntity<String> deleteEntity(@PathVariable Long key) {
+    	LOG.info("MvcRestController hit.  deleteEntity()");
+    	ResponseEntity<String> responseEntity = null;
+    	if(service.containsKey(key)) {
+    		service.remove(key);
+    		responseEntity = new ResponseEntity<String>(HttpStatus.OK);
+    	} else {
+    		// Key not found.
+    		responseEntity = new ResponseEntity<String>(HttpStatus.NOT_FOUND);
+    	}
+    	return responseEntity;
     }
 }
--- webappdemo/src/test/groovy/com/aztec/webappdemo/controller/MvcRestControllerIntegrationTest.groovy
+++ webappdemo/src/test/groovy/com/aztec/webappdemo/controller/MvcRestControllerIntegrationTest.groovy
@@ -2,9 +2,14 @@ package com.aztec.webappdemo.controller;
 
 import groovy.util.GroovyTestCase;
 
+import groovyx.net.http.HttpResponseException;
+import com.aztec.util.MarshallingUtil;
+import com.aztec.webappdemo.types.Entity;
+
 class MvcRestControllerIntegrationTest extends GroovyTestCase {
 	
-	def URL = "http://localhost:9090/webappdemo/mvc/rest/";
+	def URL = "http://localhost:9090/webappdemo/";
+	def PATH = "mvc/rest/entity/";
 	
 	/**
 	 * Create an entity.  201 CREATED expected.
@@ -16,11 +21,90 @@ class MvcRestControllerIntegrationTest extends GroovyTestCase {
         def body = '{"key": ' + key + ', "value": "' + value + '"}' 
 		println("createEntityBody=" + body)
 		
-		def response = new groovyx.net.http.RESTClient(URL).post(path: "entity/", contentType: groovyx.net.http.ContentType.JSON, body: body)
+		def response = new groovyx.net.http.RESTClient(URL).post(path: PATH, contentType: groovyx.net.http.ContentType.JSON, body: body)
 
 		assertEquals(201, response.status)
 
 		/* check that the resource location is returned */
-		assertTrue(response.headers["Location"].getValue().contains("/mvc/rest/entity/"+key))
+		assertTrue(response.headers["Location"].getValue().contains(PATH+key))
+	}
+	
+	/**
+	 * Get an entity. 200 OK expected.
+	 */
+	def void testGetEntity() {
+		// First create the entity.
+		long key = 111;
+		String value = "value";
+		def body = '{"key": ' + key + ', "value": "' + value + '"}'		
+		def responseFromCreate = new groovyx.net.http.RESTClient(URL).post(path: PATH, contentType: groovyx.net.http.ContentType.JSON, body: body)
+		assertEquals(201, responseFromCreate.status)
+		
+		// Now test the GET.
+		def currentUrl = responseFromCreate.headers["Location"].getValue()
+		currentUrl = currentUrl.substring(1)
+		println "currentUrl:" + currentUrl
+		def responseFromGet = new groovyx.net.http.RESTClient(URL).get(path: currentUrl);
+		assertEquals(200, responseFromGet.status)
+		Entity entity = MarshallingUtil.unmarshalJson(responseFromGet.data.toString(), Entity.class);
+		assertEquals(key, entity.getKey())
+		assertEquals(value, entity.getValue())
+	}
+	
+	/**
+	 * Update an entity.  200 OK expected.
+	 */
+	def void testUpdateEntity() {
+		// First create the entity.
+		long key = 111;
+		String value = "value";
+		def body = '{"key": ' + key + ', "value": "' + value + '"}'
+		def responseFromCreate = new groovyx.net.http.RESTClient(URL).post(path: PATH, contentType: groovyx.net.http.ContentType.JSON, body: body)
+		assertEquals(201, responseFromCreate.status)
+		
+		// Now test the UPDATE.
+		def currentUrl = responseFromCreate.headers["Location"].getValue()
+		currentUrl = currentUrl.substring(1)
+		String valueUpdated = "value_updated";
+		body = '{"key": ' + key + ', "value": "' + valueUpdated + '"}'
+		def responseFromUpdate = new groovyx.net.http.RESTClient(URL).put(path: currentUrl, contentType: groovyx.net.http.ContentType.JSON, body: body)
+		assertEquals(200, responseFromUpdate.status)
+		
+		// GET the updated record to assert.
+		def responseFromGet = new groovyx.net.http.RESTClient(URL).get(path: currentUrl);
+		assertEquals(200, responseFromGet.status)
+		Entity entity = MarshallingUtil.unmarshalJson(responseFromGet.data.toString(), Entity.class);
+		assertEquals(key, entity.getKey())
+		assertEquals(valueUpdated, entity.getValue())
+	}
+	
+	/**
+	 * Delete an entity.  200 OK expected.
+	 */
+	def void testDeleteEntity() {
+		// First create the entity.
+		long key = 111;
+		String value = "value";
+		def body = '{"key": ' + key + ', "value": "' + value + '"}'
+		def responseFromCreate = new groovyx.net.http.RESTClient(URL).post(path: PATH, contentType: groovyx.net.http.ContentType.JSON, body: body)
+		assertEquals(201, responseFromCreate.status)
+		
+		// Assert the entity is found.
+		def currentUrl = responseFromCreate.headers["Location"].getValue()
+		currentUrl = currentUrl.substring(1)
+		def responseFromGet = new groovyx.net.http.RESTClient(URL).get(path: currentUrl);
+		assertEquals(200, responseFromGet.status)
+		
+		// Now DELETE the entity.
+		def responseFromDelete = new groovyx.net.http.RESTClient(URL).delete(path: currentUrl)
+		assertEquals(200, responseFromDelete.status)
+		
+		// Assert the entity is now not found.
+		try {
+			def responseFromNextGet = new groovyx.net.http.RESTClient(URL).get(path: currentUrl);
+			fail("Exception should be thrown.");
+		} catch(HttpResponseException e){
+			assertEquals(404, e.getResponse().status);
+		}
 	}
 }
