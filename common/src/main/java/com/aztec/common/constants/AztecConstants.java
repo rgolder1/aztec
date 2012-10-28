03d73f8df98c724fc65463b031c70f2d4565c489
Rob Golder <rgolder1@yahoo.com>
2012-10-28 13:00:08 +0000

    Refactoring demo projects.

---
+++ common/src/main/java/com/aztec/common/constants/AztecConstants.java
@@ -0,0 +1,7 @@
+package com.aztec.common.constants;
+
+public class AztecConstants {
+
+    public static final String RESPONSE_SUCCESS = "SUCCESS";
+    public static final String RESPONSE_FAILED = "FAILED";
+}
